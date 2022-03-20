b8aa796b5cd5c7c88e9c589d382a40d65b099e32
DphnZwp <zwuupdaphne@gmail.com>
2022-03-20 15:26:32 +0100

    Api code verbeterd

--- scripts/api.js
+++ scripts/api.js
@@ -5,13 +5,12 @@ const toggleButtonDelete = document.querySelector('.toggle-form-delete')
 const closeButtonPost = document.querySelector('.close-button-post')
 const closeButtonPut = document.querySelector('.close-button-put')
 const closeButtonDelete = document.querySelector('.close-button-delete')
-
 // Post variables
 const apiUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
 const smartzonePostForm = document.querySelector('form#smartzonePostForm')
 const smartzonePutForm = document.querySelector('form#smartzonePutForm')
 const smartzoneDeleteForm = document.querySelector('form#smartzoneDeleteForm')
-
+// Toggle buttons show form
 toggleButtonPost.addEventListener('click', () => {
   smartzonePostForm.classList.toggle('form-show')
 })
@@ -23,7 +22,7 @@ toggleButtonPut.addEventListener('click', () => {
 toggleButtonDelete.addEventListener('click', () => {
   smartzoneDeleteForm.classList.toggle('form-show')
 })
-
+// Close buttons form
 closeButtonPost.addEventListener('click', () => {
   smartzonePostForm.classList.remove('form-show')
 })
@@ -40,7 +39,7 @@ smartzonePostForm.addEventListener('submit', (post) => {
   post.preventDefault()
 
   let data = {
-    smartzoneId: document.querySelector('#smartzoneId').value,
+    smartzoneId: Number(document.querySelector('#smartzoneId').value),
     name: document.querySelector('#name').value,
     town: document.querySelector('#town').value,
     location: document.querySelector('#location').value,
@@ -61,27 +60,52 @@ smartzonePostForm.addEventListener('submit', (post) => {
   })
 })
 
-smartzonePutForm.addEventListener('submit', (put) => {
-  post.preventDefault()
+// smartzonePutForm.addEventListener('submit', (put) => {
+//   put.preventDefault()
 
-  let data = {
-    smartzoneId: document.querySelector('#smartzoneId').value,
-    name: document.querySelector('#name').value,
-    town: document.querySelector('#town').value,
-    location: document.querySelector('#location').value,
-    function: document.querySelector('#function').value,
-    time: document.querySelector('#time').value,
-    size: document.querySelector('#size').value,
-    utilization: document.querySelector('#utilization').value,
-    description: document.querySelector('#description').value,
-    image: document.querySelector('#image').value,
-  }
+//   let data = {
+//     smartzoneId: Number(document.querySelector('#smartzoneId').value),
+//     name: document.querySelector('#name').value,
+//     town: document.querySelector('#town').value,
+//     location: document.querySelector('#location').value,
+//     function: document.querySelector('#function').value,
+//     time: document.querySelector('#time').value,
+//     size: document.querySelector('#size').value,
+//     utilization: document.querySelector('#utilization').value,
+//     description: document.querySelector('#description').value,
+//     image: document.querySelector('#image').value,
+//   }
 
-  fetch(apiUrl, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-})
+//   fetch(apiUrl, {
+//     method: 'PUT',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   })
+// })
+
+// smartzonedeleteForm.addEventListener('submit', (delete) => {
+//   delete.preventDefault()
+
+//   let data = {
+//     smartzoneId: Number(document.querySelector('#smartzoneId').value),
+//     name: document.querySelector('#name').value,
+//     town: document.querySelector('#town').value,
+//     location: document.querySelector('#location').value,
+//     function: document.querySelector('#function').value,
+//     time: document.querySelector('#time').value,
+//     size: document.querySelector('#size').value,
+//     utilization: document.querySelector('#utilization').value,
+//     description: document.querySelector('#description').value,
+//     image: document.querySelector('#image').value,
+//   }
+
+//   fetch(apiUrl, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   })
+// })
